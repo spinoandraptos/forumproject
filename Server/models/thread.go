c33d5d699c4fb3f829ffdabe46ceb42cc94221ed
spinoandraptos <spinoandraptos@gmail.com>
2022-12-17 14:43:55 +0800

    updated threads

--- Server/models/thread.go
+++ Server/models/thread.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"log"
+	"time"
+)
 
 // a forum thread has an unique ID, title, content, category ID, author, author's ID and records the time of its creation or update
 type Thread struct {
@@ -12,4 +16,29 @@ type Thread struct {
 	CategoryID uint32 `json:"categoryid"`
 	CreatedAt  time.Time
 	ModifiedAt time.Time
+}
+
+var DB *sql.DB
+
+// to retrieve all created threads from the database
+// we select every row from the threads table and sort by most recent thread on top
+// then we store every row in a thread and append the thread to a slice of threads
+// finally we and close the database connection
+func retrieveAllThreads() (threadSlice []Thread, err error) {
+	posts, err := DB.Query("SELECT * FROM threads ORDER BY CreatedAt DESC")
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	for posts.Next() {
+		var thread Thread
+		err = posts.Scan(&thread.ID, &thread.Title, &thread.Content, &thread.AuthorID, &thread.CategoryID, &thread.CreatedAt)
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+		threadSlice = append(threadSlice, thread)
+	}
+	posts.Close()
+	return
 }
